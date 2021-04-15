06c26cfb71981c08f0ba69cef74333f1d9f0305e
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-14 20:57:53 -0600

    - Finished

--- FirebaseDemo/FirebaseDemoApp.swift
+++ FirebaseDemo/FirebaseDemoApp.swift
@@ -6,12 +6,43 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FirebaseDemoApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  
+  init() {
+    FirebaseApp.configure()
+    
+    MakeReservation()
+  }
+  
+  func MakeReservation() {
+    
+    // Declare reference to the cloud Firestore DB
+    let db = Firestore.firestore()
+    
+    // Reference to the reservations collection. Will be created if it doesn't already exist
+    let reservations = db.collection("reservations")
+    
+    // Option 1: Create a document with a given identifier (i.e. ID)
+    reservations.document("test123")
+      .setData(["name": "Carol", "people": 22])
+    
+    // Option 2: Create a document with a unique identifier (i.e. auto-generated ID)
+    reservations.document()
+      .setData(["name": "Tom"])
+    
+    // Option 3: Create a document with given data
+    let document = reservations.addDocument(data: ["name": "Sue", "people": 10])
+    
+    // TODO: Work with the document
+    //document.
+  }
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }

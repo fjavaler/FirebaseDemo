877a2de6d1c5b555c88e1573dfca679c069265a6
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-14 20:06:44 -0600

    Initial Commit

---
+++ FirebaseDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseDemo
+//
+//  Created by Fred Javalera on 4/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseDemo/FirebaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseDemoApp.swift
+//  FirebaseDemo
+//
+//  Created by Fred Javalera on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
